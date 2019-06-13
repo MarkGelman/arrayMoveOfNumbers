39f9fa22d5bc54a5ee47b867d8cf54170734d46d
MarkGelman <47790935+MarkGelman@users.noreply.github.com>
2019-06-13 15:39:46 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrayMoveOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers1 = new int[3];
+            
+            for (int i = 0; i < 3; i++)
+            {
+                numbers1[i] = int.Parse(Console.ReadLine());
+               
+            }
+            Console.WriteLine("");
+
+            int intermediate = 0;
+
+            intermediate = numbers1[0]; numbers1[0] = numbers1[1]; numbers1[1] = numbers1[2]; numbers1[2] = intermediate;
+
+            Console.WriteLine($"[ {numbers1[0]} {numbers1[1]} {numbers1[2]} ]");
+
+            Console.ReadKey();
+        }
+    }
+}
